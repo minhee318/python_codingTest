712a139061667f2cac68172ac6068279bcb02461
강민희 <52806967+minhee318@users.noreply.github.com>
2021-01-29 21:22:01 +0900

    1/29
    구현 연습문제 2개

---
+++ 구현/시각.py
@@ -0,0 +1,9 @@
+num = int(input())
+
+count = 0
+for h in range(num+1):
+    for m in range(60):
+        for s in range(60):
+            if '3' in str(h)+str(m)+str(s):
+                count +=1
+print(count)                
